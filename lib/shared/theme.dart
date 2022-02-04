277816d7a91dae8a00688d17e0e122a981f59576
Christ Henry Loha <cristloha2205@gmail.com>
2022-02-04 22:14:29 +0800

    UI APP GELOKOS

---
+++ lib/shared/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0xff29D5F8);
+Color kBlackColor = const Color(0xff040507);
+Color kGreyColor = const Color(0xffA5A5A5);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
