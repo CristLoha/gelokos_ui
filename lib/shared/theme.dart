508377d901983dfef6d56df4fc621271d1a4651a
Christ Henry Loha <cristloha2205@gmail.com>
2022-02-04 22:23:57 +0800

    update THEME

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 Color kPrimaryColor = const Color(0xff29D5F8);
 Color kBlackColor = const Color(0xff040507);
 Color kGreyColor = const Color(0xffA5A5A5);
+Color kBackgroundColor = const Color(0xffE5E5E5);
 
 TextStyle blackTextStyle = GoogleFonts.poppins(
   color: kBlackColor,
