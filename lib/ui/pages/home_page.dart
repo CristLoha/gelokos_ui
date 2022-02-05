0bdb6d5b27fe34ed89a8233c1e03666fabcc1e0d
Christ Henry Loha <cristloha2205@gmail.com>
2022-02-05 16:49:04 +0800

    add SEARCH BOX

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../shared/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,9 +7,85 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 22,
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset('assets/Icon_menu_outline.svg'),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  top: 33,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello,',
+                      style: blackTextStyle.copyWith(
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      'Shayna Far',
+                      style: blackTextStyle.copyWith(
+                        fontWeight: bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Image.asset(
+              'assets/user_pic.png',
+              width: 55,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget searchBox() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22),
+        child: Container(
+          width: 331,
+          height: 55,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 16,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(11),
+          ),
+          child: TextFormField(
+            decoration: InputDecoration.collapsed(
+              hintText: 'Find your next home',
+              hintStyle: greyTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: light,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: ListView(),
+      body: ListView(
+        children: [
+          header(),
+          searchBox(),
+        ],
+      ),
     );
   }
 }
