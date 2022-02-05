3c3baf283015788ddfff0edb5eccd789ecd09575
Christ Henry Loha <cristloha2205@gmail.com>
2022-02-05 16:48:53 +0800

    add COLOR

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 Color kPrimaryColor = const Color(0xff29D5F8);
 Color kBlackColor = const Color(0xff040507);
+Color kWhiteColor = const Color(0xffFFFFFF);
 Color kGreyColor = const Color(0xffA5A5A5);
 Color kBackgroundColor = const Color(0xffE5E5E5);
 
@@ -11,7 +12,7 @@ TextStyle blackTextStyle = GoogleFonts.poppins(
 );
 
 TextStyle greyTextStyle = GoogleFonts.poppins(
-  color: kBlackColor,
+  color: kGreyColor,
 );
 
 FontWeight light = FontWeight.w300;
